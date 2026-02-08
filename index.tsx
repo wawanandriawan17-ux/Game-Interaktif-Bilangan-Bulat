238afcff2da9dd57fae521a1a01498ba23eab221
wawanandriawan17-ux <wawanandriawan17@guru.smp.belajar.id>
2026-02-08 11:48:30 +0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,16 +1,15 @@
-
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 
+// Mengambil elemen 'root' dari index.html
 const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
